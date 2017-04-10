a61c2b312c297bf25c79e99d3637bcdabaaef058
Joao Maximo <Joao Maximo>
2017-04-10 16:46:26 +0100

    nothing relevant

--- so/Source.cpp
+++ so/Source.cpp
@@ -9,33 +9,49 @@
 #include <array>
 
 struct myHandle {
-	PVOID base;
-	PVOID allocationBase;
+	PVOID startAllocationBase;
+	PVOID currBlockBase;
 	SIZE_T totalsize;
-}; typedef myHandle;
+} typedef myHandle;
+int printProcessInfo(HANDLE handle);
 
 void  main(int argc, char* argv[]) {
 
-	myHandle curr;
-	myHandle biggest;
-	
+//	myHandle curr;
+//	myHandle biggest;
+
+
 	HANDLE handle;
 	if (argc > 1 )
 		handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , false,  atoi(argv[1]));
+//	handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, 12016);
 	else
 		handle = GetCurrentProcess();
 
-	PMEMORY_BASIC_INFORMATION info;
+	MEMORY_BASIC_INFORMATION info;
 
-	VirtualQueryEx(handle, NULL, info, MAX_PATH);
+	VirtualQueryEx(handle, NULL, &info, MAX_PATH);
 
-	if (info->State != MEM_RESERVE) {
-		info->RegionSize += curr.totalsize;
+	
+/*
+	info -> BaseAddress
+	info -> AllocationBase
+	info -> RegionSize
+	info -> State
+*/
+	printProcessInfo(handle);
+	printf("Region Base Allocation %x \n", info.AllocationBase);
+	printf("size %x \n", info.RegionSize);
 
+	CloseHandle(handle);
+	getchar();
+}
+
+
+int printProcessInfo(HANDLE handle) {
+	TCHAR name[MAX_PATH];
+	if (GetModuleFileNameEx(handle, 0, name, MAX_PATH))	{
+		printf("Process Name: %s \n", name);
 	}
-
-
-
-
-
+	return 0;
 }
