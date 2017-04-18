9e007e8244a71ba62f266a45c0b818f3192e775b
Baltas93 <d.carvalho@live.com.pt>
2017-04-18 16:14:53 +0100

    the beginning of exe5

---
+++ so/Exe5.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <ctype.h>
+
+
+#ifdef _WIN32
+#include <sys/utime.h>
+#else
+#include <utime.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <limits.h>
+#endif
+
+#include "jhead.h"
+void main(){
+
+}	
